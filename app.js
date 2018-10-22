35f37036da7531442938909ef300a0a7cd64bd82
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-10-23 01:05:34 +0300

    Namespaces

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get("/", function(req, res) {
-  res.sendfile("index.html");
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
 });
 
-var clients = 0;
-io.on("connection", function(socket) {
-  clients++;
-  socket.emit("newclientconnect", { description: "Hey, welcome!" }); //kendisine gider
-  socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'}) // kendisi haric herkese gider
-  socket.on('disconnect', function () {
-    clients--;
-    socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
- });
+var nsp = io.of('/my-namespace');
+nsp.on('connection', function(socket) {
+   console.log('someone connected');
+   nsp.emit('hi', 'Hello everyone!');
 });
 
 http.listen(3000, function() {
-  console.log("listening on localhost:3000");
+   console.log('listening on localhost:3000');
 });
