933e73e497b042085c33be10c8989e5a02d6bd8f
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-10-27 14:06:13 +0300

    Rooms

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.get('/', function(req, res) {
-   res.sendfile('index.html');
+app.get("/", function(req, res) {
+  res.sendfile("index.html");
 });
 
-var nsp = io.of('/my-namespace');
-nsp.on('connection', function(socket) {
-   console.log('someone connected');
-   nsp.emit('hi', 'Hello everyone!');
+var roomno = 1;
+io.on("connection", function(socket) {
+  //Increase roomno 2 clients are present in a room.
+  if (
+    io.nsps["/"].adapter.rooms["room-" + roomno] &&
+    io.nsps["/"].adapter.rooms["room-" + roomno].length > 1
+  )
+    roomno++;
+  socket.join("room-" + roomno);
+
+  //Send this event to everyone in the room.
+  io.sockets
+    .in("room-" + roomno)
+    .emit("connectToRoom", "You are in room no. " + roomno);
 });
 
 http.listen(3000, function() {
-   console.log('listening on localhost:3000');
+  console.log("listening on localhost:3000");
 });
